28070a27ef63f64d6a0385bd79f989a0a85d581a
Csaba Pap <csabapapdev@gmail.com>
2017-12-05 21:42:31 +0100

    get and display trending shows

--- app/src/main/java/hu/csabapap/seriesreminder/ui/main/home/HomeFragment.kt
+++ app/src/main/java/hu/csabapap/seriesreminder/ui/main/home/HomeFragment.kt
@@ -1,14 +1,19 @@
 package hu.csabapap.seriesreminder.ui.main.home
 
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dagger.android.support.DaggerFragment
 import hu.csabapap.seriesreminder.R
+import hu.csabapap.seriesreminder.ui.adapters.TrendingShowsAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
 import javax.inject.Inject
 
 
@@ -17,6 +22,9 @@ class HomeFragment : DaggerFragment() {
     @Inject
     lateinit var mainViewModelProvider: ViewModelProvider.Factory
     private lateinit var homeViewModel: HomeViewModel
+
+    lateinit var layoutManager: LinearLayoutManager
+    private val trendingShowsAdapter = TrendingShowsAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +36,33 @@ class HomeFragment : DaggerFragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater!!.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        homeViewModel.trendingShowsLiveData.observe(this, Observer {
+            it?.apply {
+                trendingShowsAdapter.shows = it
+            }
+        })
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        layoutManager = shows_grid.layoutManager as LinearLayoutManager
+        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+
+        shows_grid.apply {
+            adapter = trendingShowsAdapter
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        homeViewModel.getTrendingShows()
     }
 
 }
--- app/src/main/java/hu/csabapap/seriesreminder/ui/main/home/HomeViewModel.kt
+++ app/src/main/java/hu/csabapap/seriesreminder/ui/main/home/HomeViewModel.kt
@@ -1,10 +1,37 @@
 package hu.csabapap.seriesreminder.ui.main.home
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.util.Log
+import android.view.View
 import hu.csabapap.seriesreminder.data.ShowsRepository
+import hu.csabapap.seriesreminder.data.db.entities.SRShow
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-class HomeViewModel @Inject constructor(val showsRepository: ShowsRepository) : ViewModel() {
+class HomeViewModel @Inject constructor(private val showsRepository: ShowsRepository) : ViewModel() {
 
+    val compositeDisposable = CompositeDisposable()
 
+    val trendingShowsLiveData = MutableLiveData<List<SRShow>>()
+
+    fun getTrendingShows() {
+         val disposable = showsRepository.getTrendingShows()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    trendingShowsLiveData.value = it
+                },{})
+
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
 }
