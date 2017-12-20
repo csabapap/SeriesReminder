1e7e7f72a74a7a9d70ebac2e30c6de751c860bdc
Csaba Pap <csabapapdev@gmail.com>
2017-12-20 21:37:58 +0100

    limit 10

--- app/src/main/java/hu/csabapap/seriesreminder/data/db/daos/TrendingDao.kt
+++ app/src/main/java/hu/csabapap/seriesreminder/data/db/daos/TrendingDao.kt
@@ -7,6 +7,7 @@ import android.arch.persistence.room.Query
 import hu.csabapap.seriesreminder.data.db.entities.SRTrendingShow
 import hu.csabapap.seriesreminder.data.db.entities.TrendingGridItem
 import io.reactivex.Flowable
+import org.intellij.lang.annotations.Language
 
 @Dao
 interface TrendingDao {
@@ -14,7 +15,8 @@ interface TrendingDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(trendingShow: SRTrendingShow)
 
-    @Query("SELECT * FROM trending_shows ORDER BY watchers DESC")
+    @Language("RoomSql")
+    @Query("SELECT * FROM trending_shows ORDER BY watchers DESC LIMIT 10")
     fun getTrendingShows() : Flowable<List<TrendingGridItem>>
 
 }
