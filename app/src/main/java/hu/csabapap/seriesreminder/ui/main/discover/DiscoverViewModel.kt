1d2847ede89cbbe173605accdbf3ef2a8329195d
Csaba Pap <csabapapdev@gmail.com>
2018-01-17 21:44:34 +0100

    rxkotlin

--- app/src/main/java/hu/csabapap/seriesreminder/ui/main/discover/DiscoverViewModel.kt
+++ app/src/main/java/hu/csabapap/seriesreminder/ui/main/discover/DiscoverViewModel.kt
@@ -6,12 +6,15 @@ import hu.csabapap.seriesreminder.data.ShowsRepository
 import hu.csabapap.seriesreminder.data.db.entities.GridItem
 import hu.csabapap.seriesreminder.data.db.entities.Item
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 import javax.inject.Inject
 
 class DiscoverViewModel @Inject constructor(private val showsRepository: ShowsRepository)
     : ViewModel() {
-
+    private val disposables = CompositeDisposable()
     val itemsLiveData = MutableLiveData<List<GridItem<Item>>>()
 
     fun getItems(type: Int) {
@@ -21,15 +24,20 @@ class DiscoverViewModel @Inject constructor(private val showsRepository: ShowsRe
     }
 
     private fun getTrendingShows() {
-        showsRepository.getTrendingShows(20)
+        disposables += showsRepository.getTrendingShows(20)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
+                .subscribe ({
                     itemsLiveData.value = it as List<GridItem<Item>>
-                }
+                }, { Timber.e(it) })
     }
 
     private fun getPopularShows() {
 
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
     }
 }
