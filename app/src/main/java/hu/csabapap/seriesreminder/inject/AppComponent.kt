db79ef0d563f80e78623bf35be6f5cf28cb4c6f7
Csaba Pap <pap.csaba@zengo.eu>
2018-01-25 09:18:45 +0100

    di fix

--- app/src/main/java/hu/csabapap/seriesreminder/inject/AppComponent.kt
+++ app/src/main/java/hu/csabapap/seriesreminder/inject/AppComponent.kt
@@ -16,7 +16,6 @@ import javax.inject.Singleton
         modules = [
             AndroidSupportInjectionModule::class,
             AppModule::class,
-            ActivityBindingModule::class,
             ApiModule::class,
             SrViewModelModule::class,
             HomeModule::class,
