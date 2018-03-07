b2d39f6f95a47183c37dd47a8aceecdc3fe35133
Csaba Pap <csabapapdev@gmail.com>
2018-03-07 22:50:29 +0100

    default empty string if episode image is not available

--- app/src/main/java/hu/csabapap/seriesreminder/data/network/entities/TvdbEpisode.kt
+++ app/src/main/java/hu/csabapap/seriesreminder/data/network/entities/TvdbEpisode.kt
@@ -1,9 +1,12 @@
 package hu.csabapap.seriesreminder.data.network.entities
 
+import se.ansman.kotshi.JsonDefaultValueString
 import se.ansman.kotshi.JsonSerializable
 
 @JsonSerializable
 data class EpisodeData(val data: TvdbEpisode)
 
 @JsonSerializable
-data class TvdbEpisode(val filename: String = "")
+data class TvdbEpisode(
+        @JsonDefaultValueString(value = "")
+        val filename: String)
