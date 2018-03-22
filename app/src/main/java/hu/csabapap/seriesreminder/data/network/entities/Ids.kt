02abe219a61b873744ebe925133cf8e4c2160705
Csaba Pap <csabapapdev@gmail.com>
2018-03-22 17:42:56 +0100

    default tvdb id

--- app/src/main/java/hu/csabapap/seriesreminder/data/network/entities/Ids.kt
+++ app/src/main/java/hu/csabapap/seriesreminder/data/network/entities/Ids.kt
@@ -1,6 +1,10 @@
 package hu.csabapap.seriesreminder.data.network.entities
 
+import se.ansman.kotshi.JsonDefaultValueInt
 import se.ansman.kotshi.JsonSerializable
 
 @JsonSerializable
-data class Ids (var trakt: Int, var tvdb: Int)
+data class Ids (
+        var trakt: Int,
+        @JsonDefaultValueInt(value = 0)
+        var tvdb: Int)
