bfa6dbd3e6e054959c9bed80df3e248af90fffc5
Csaba Pap <csabapapdev@gmail.com>
2018-03-23 19:13:59 +0100

    default values update

--- app/src/main/java/hu/csabapap/seriesreminder/data/network/entities/Airs.kt
+++ app/src/main/java/hu/csabapap/seriesreminder/data/network/entities/Airs.kt
@@ -1,57 +1,13 @@
 package hu.csabapap.seriesreminder.data.network.entities
 
-import se.ansman.kotshi.JsonDefaultValue
+import se.ansman.kotshi.JsonDefaultValueString
 import se.ansman.kotshi.JsonSerializable
-
-@Target(AnnotationTarget.VALUE_PARAMETER,
-        AnnotationTarget.FUNCTION,
-        AnnotationTarget.CONSTRUCTOR,
-        AnnotationTarget.FIELD,
-        AnnotationTarget.PROPERTY_GETTER)
-@MustBeDocumented
-@Retention(AnnotationRetention.SOURCE)
-@JsonDefaultValue // Makes this annotation a custom default value annotation
-annotation class DefaultDay
-
-@Target(AnnotationTarget.VALUE_PARAMETER,
-        AnnotationTarget.FUNCTION,
-        AnnotationTarget.CONSTRUCTOR,
-        AnnotationTarget.FIELD,
-        AnnotationTarget.PROPERTY_GETTER)
-@MustBeDocumented
-@Retention(AnnotationRetention.SOURCE)
-@JsonDefaultValue // Makes this annotation a custom default value annotation
-annotation class DefaultTime
-
-@Target(AnnotationTarget.VALUE_PARAMETER,
-        AnnotationTarget.FUNCTION,
-        AnnotationTarget.CONSTRUCTOR,
-        AnnotationTarget.FIELD,
-        AnnotationTarget.PROPERTY_GETTER)
-@MustBeDocumented
-@Retention(AnnotationRetention.SOURCE)
-@JsonDefaultValue // Makes this annotation a custom default value annotation
-annotation class DefaultTimeZone
 
 @JsonSerializable
 data class Airs(
-        @DefaultDay
+        @JsonDefaultValueString(value = "")
         val day: String = "",
-        @DefaultTime
+        @JsonDefaultValueString(value = "")
         val time: String = "",
-        @DefaultTimeZone
-        val timezone: String = "") {
-    companion object {
-        @DefaultDay
-        @JvmField
-        var defaultDay = ""
-
-        @DefaultTime
-        @JvmField
-        var defaultTime = ""
-
-        @DefaultTimeZone
-        @JvmField
-        var defaultTimeZone = ""
-    }
-}
+        @JsonDefaultValueString(value = "")
+        val timezone: String = "")
