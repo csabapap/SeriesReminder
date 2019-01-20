3641e945d78ef0beabcfc07c19ed3b21a75597c3
Csaba Pap <csabapapdev@gmail.com>
2019-01-20 18:26:27 +0100

    display notification from worker

--- app/src/main/java/hu/csabapap/seriesreminder/services/workers/ShowReminderWorker.kt
+++ app/src/main/java/hu/csabapap/seriesreminder/services/workers/ShowReminderWorker.kt
@@ -1,13 +1,18 @@
 package hu.csabapap.seriesreminder.services.workers
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import androidx.core.os.bundleOf
+import androidx.core.app.NotificationCompat
 import androidx.work.Result
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import hu.csabapap.seriesreminder.services.ReminderService
+import hu.csabapap.seriesreminder.R
+import hu.csabapap.seriesreminder.ui.showdetails.ShowDetailsActivity
 import hu.csabapap.seriesreminder.utils.Reminder
+import hu.csabapap.seriesreminder.utils.ShowDetails
+import hu.csabapap.seriesreminder.utils.createChannel
 
 class ShowReminderWorker(context: Context, workerParameters: WorkerParameters)
     : Worker(context, workerParameters) {
@@ -18,13 +23,26 @@ class ShowReminderWorker(context: Context, workerParameters: WorkerParameters)
         if (showId == -1) {
             return Result.failure()
         }
-        val title = inputData.getString(Reminder.SHOW_TITLE)
+        val title = inputData.getString(Reminder.SHOW_TITLE) ?: return Result.failure()
 
-        val serviceIntent = Intent(applicationContext, ReminderService::class.java)
-        serviceIntent.putExtras(bundleOf(Reminder.SHOW_ID to showId,
-                Reminder.SHOW_TITLE to title))
-        applicationContext.startService(serviceIntent)
+        displayNotification(showId, title)
 
         return Result.success()
+    }
+
+    private fun displayNotification(id: Int, title: String) {
+        val resultIntent = Intent(applicationContext, ShowDetailsActivity::class.java)
+        resultIntent.putExtra(ShowDetails.EXTRA_SHOW_ID, id)
+        val resultPendingIntent = PendingIntent.getActivity(applicationContext, 17,
+                resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val builder = NotificationCompat.Builder(applicationContext, "episode_reminder")
+        builder.setContentTitle(title)
+        builder.setSmallIcon(R.drawable.ic_notifications_black_24dp)
+        builder.setContentIntent(resultPendingIntent)
+        builder.createChannel()
+
+        val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+        notificationManager.notify(id, builder.build())
     }
 }
