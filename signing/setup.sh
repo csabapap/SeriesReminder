e59d1b1b9ad9ff30f12ab5c0fdf1f7cf3977ed7c
Csaba Pap <csabapapdev@gmail.com>
2020-10-04 16:06:18 +0200

    setup script fix

--- signing/setup.sh
+++ signing/setup.sh
@@ -3,4 +3,4 @@
 ENCRYPT_KEY=$1
 
 #decrypt services file
-openssl aes-256-cbc -md sha256 -d -in signing/google-services.aes -out app/google-services.json -k "$ENCRYPT_KEY"
+openssl aes-256-cbc -md sha256 -d -in signing/google-services.aes -out app/google-services.json -k $ENCRYPT_KEY
