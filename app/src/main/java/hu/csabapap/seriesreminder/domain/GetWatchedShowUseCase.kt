6b553d09d8999fab97d2f0e2370be219ad3ef72b
Csaba Pap <csabapap@users.noreply.github.com>
2022-03-26 16:04:38 +0100

    get watched shows data asynchronously and save them in transactions

---
+++ app/src/main/java/hu/csabapap/seriesreminder/domain/GetWatchedShowUseCase.kt
@@ -0,0 +1,56 @@
+package hu.csabapap.seriesreminder.domain
+
+import hu.csabapap.seriesreminder.data.SeasonsRepository
+import hu.csabapap.seriesreminder.data.db.entities.SRSeason
+import hu.csabapap.seriesreminder.data.db.entities.SRShow
+import hu.csabapap.seriesreminder.data.network.TvdbApi
+import hu.csabapap.seriesreminder.data.network.entities.Image
+import hu.csabapap.seriesreminder.data.repositories.shows.ShowsRepository
+import timber.log.Timber
+import javax.inject.Inject
+
+class GetWatchedShowUseCase @Inject constructor(
+        private val showsRepository: ShowsRepository,
+        private val seasonsRepository: SeasonsRepository,
+        private val tvdbApi: TvdbApi
+) {
+
+    suspend operator fun invoke(showId: Int): ShowWithSeasonsAndImagesFromTrakt? {
+        val show = showsRepository.getShow(showId) ?: return null
+        val posters = tvdbApi.images(show.tvdbId, "poster") ?: return null
+        val covers = try {
+            tvdbApi.images(show.tvdbId, "fanart")
+        } catch (e: Exception) {
+            Timber.e(e)
+            null
+        }
+
+        val popularPoster = posters.data.maxByOrNull { image ->
+            image.ratingsInfo.average
+        }
+        val popularCover = covers?.data?.maxByOrNull { image ->
+            image.ratingsInfo.average
+        }
+
+        val newShow = show.copy(
+                poster = popularPoster?.fileName ?: "",
+                posterThumb = popularPoster?.thumbnail ?: "",
+                cover = popularCover?.fileName ?: "",
+                coverThumb = popularCover?.thumbnail ?: "")
+
+        val seasonsFromWeb = seasonsRepository.getSeasonsFromWeb(show.traktId) ?: return null
+        val seasonImages = seasonsRepository.getSeasonImages(newShow.tvdbId)
+
+        return ShowWithSeasonsAndImagesFromTrakt(newShow, SeasonsWithImages(showId, seasonsFromWeb, seasonImages))
+    }
+}
+
+data class ShowWithSeasonsAndImagesFromTrakt(
+        val show: SRShow,
+        val seasonsWithImages: SeasonsWithImages
+)
+
+data class SeasonsWithImages(
+        val showTraktId: Int,
+        val seasons: List<SRSeason>,
+        val seasonsImages: Map<String, Image?>)
